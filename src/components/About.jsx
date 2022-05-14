0865ea38ac06ec13d05186abda9f5e4ad0e6f23f
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-14 19:06:15 -0400

    slight text tweaks on about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,8 @@ function About() {
 
                 </div>
 
-                <h2 className="intro">Yo, what's up? I'm <span className="name">Steven.</span></h2>
-                <p className="description">I suppose this is where I tell you that I'm super passionate, love coding, and enjoy expanding my knowledge about web developing like everyone else does, right? Nah bro, I'm unlike any other developer you've ever met before. I'm a junior front end developer who absolutely loves to code and is super passionate about what I do and enjoy learning new things while making it all look good. That's all I got for you... later. </p>
+                <h2 className="intro">Hi, what's up? I'm <span className="name">Steven.</span></h2>
+                <p className="description">I suppose this is where I tell you that I'm super passionate, love coding, and enjoy expanding my knowledge about web developing like everyone else does, right? Nah, I'm unlike any other developer you've ever met before. I'm a junior front end developer who absolutely loves to code and is super passionate about what I do and enjoy learning new things while making it all look good. That's all I got for you... later. </p>
             </div>
     )    
 }
